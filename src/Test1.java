78154ee9f7aca0406d47747d0ddd225252de5b93
davut yavas <duo.land2017@gmail.com>
2020-11-24 19:34:34 +0100

    Version 1

---
+++ src/Test1.java
@@ -0,0 +1,6 @@
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("deneme");
+        System.out.println(".gitignore dosyasina hangi dosyalar gitmeyecekse onlar yazilir.");
+    }
+}
